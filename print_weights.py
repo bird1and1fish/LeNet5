791e3bad51023b5b5db43a6d3026822f0e951469
bird1and1fish <1014693892@qq.com>
2022-05-04 17:56:10 +0800

    add print weights

--- print_weights.py
+++ print_weights.py
@@ -1,7 +1,6 @@
 import torch
 from net import LeNet
 from torchsummary import summary
-from torch import nn
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = LeNet().to(device)
@@ -10,25 +9,10 @@ model.load_state_dict(torch.load("D:/ws_pytorch/LeNet5/save_model/best_model.pth
 summary(model, (1, 28, 28))
 
 for name in model.state_dict():
-    print(name)
+    print("################" + name + "################")
+    # print(model.state_dict()[name])
+    file = open("weight/" + name + ".txt", "w")
+    file.write(str(model.state_dict()[name]))
+    file.close()
 
-print("##############c1_weight###############")
-print(model.state_dict()['c1.weight'])
-print("##############c1_bias###############")
-print(model.state_dict()['c1.bias'])
-print("##############c3_weight###############")
-print(model.state_dict()['c3.weight'])
-print("##############c3_bias###############")
-print(model.state_dict()['c3.bias'])
-print("##############c5_weight###############")
-print(model.state_dict()['c5.weight'])
-print("##############c5_bias###############")
-print(model.state_dict()['c5.bias'])
-print("##############f6_weight###############")
-print(model.state_dict()['f6.weight'])
-print("##############f6_bias###############")
-print(model.state_dict()['f6.bias'])
-print("##############output_weight###############")
-print(model.state_dict()['output.weight'])
-print("##############output_bias###############")
-print(model.state_dict()['output.bias'])
+
